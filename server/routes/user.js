ef48c85c9ee5357530a9faebed518bcc7c9b66e8
quocbaobui <51372227+quocbao238@users.noreply.github.com>
2022-11-14 20:21:53 +0700

    - Api api/remove-from-cart

--- server/routes/user.js
+++ server/routes/user.js
@@ -27,7 +27,14 @@ userRouter.post('/api/add-to-cart',auth, async (req, res) => {
         }
         
         if(isProductFound){
-            let productInCart = user.cart.find((element) => element.product._id);
+
+            let productInCart = user.cart.find((element) => {
+                console.log(element.product._id); 
+                console.log(product._id);
+               return element.product._id.toString() == product._id;
+            });
+    
+            console.log(productInCart._id);
             productInCart.quantity += 1;
         }else{
             user.cart.push({product, quantity: 1});
@@ -37,6 +44,34 @@ userRouter.post('/api/add-to-cart',auth, async (req, res) => {
      user = await user.save();
      res.json(user);
 
+    } catch (error) {
+        res.status(500,json({error:error.message}));
+    }
+});
+
+
+
+// Add Product
+userRouter.delete('/api/remove-from-cart/:id',auth, async (req, res) => {
+
+    try {
+     const {id} = req.params;
+     const product = await Product.findById(id);
+     let user = await User.findById(req.user);
+
+        for(let i =0 ; i<user.cart.length; i++){
+            const _productId = user.cart[i].product._id.toString();
+            if(_productId == product._id){
+                if(user.cart[i].quantity == 1){
+                    user.cart.splice(i,1);
+                }else{
+                    user.cart[i].quantity -= 1;
+                }
+            }
+        }
+        
+     user = await user.save();
+     res.json(user);
     } catch (error) {
         res.status(500,json({error:error.message}));
     }
